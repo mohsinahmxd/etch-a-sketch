8258982ea35c44b1f00fe5c99836840976c1809d
mohsinahmxd <bmohsin785@gmail.com>
2022-05-09 22:20:54 +0100

    can now draw via mouseover

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ function sixteendivs () {
     for (let i = 1; i < 257; i++) {
         const div = document.createElement('div');
         div.className = "div" + i;
-        // div.innerText = "div" + i;
+        // div.innerText = "div" + i; removing the numbers
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "red";
+        });
         divContainer.appendChild(div);
         console.log(div)
     }
 }
+
 
 sixteendivs();
