344c759b4f87d5bb15a6897625a007eb6f79bf8e
mohsinahmxd <bmohsin785@gmail.com>
2022-05-11 23:33:20 +0100

    can now remove the grid

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const divContainer = document.querySelector('.div-container')
-const divChildren = divContainer.childNodes;
+// const divChildren = divContainer.childNodes;
 
 
 function sixteendivs () {
@@ -17,6 +17,19 @@ function sixteendivs () {
 function changeLayout () {
     let uservalue = prompt("Enter new layout value")
     console.log(uservalue)
+
+    while (divContainer.firstElementChild) { // while a firstelementchild exists
+        divContainer.removeChild(divContainer.firstElementChild); // remove the first element child
+    }
+    
+
+
+    // create new grid
+    for (let i = 1; i < uservalue; i++) {
+        const div = document.createElement('div');
+        div.className = "div" + i;
+        divContainer.appendChild(div);
+    }
 
 }
 
