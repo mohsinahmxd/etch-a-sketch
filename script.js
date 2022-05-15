c77875c2fe76f1ea2dfed2d639da4fab297f95f5
mohsinahmxd <bmohsin785@gmail.com>
2022-05-15 13:21:19 +0100

    can now resize and generate new grid

--- script.js
+++ script.js
@@ -1,36 +1,53 @@
 const divContainer = document.querySelector('.div-container')
-// const divChildren = divContainer.childNodes;
 
 
 function sixteendivs () {
     for (let i = 1; i < 257; i++) {
         const div = document.createElement('div');
         div.className = "div" + i;
-        // div.innerText = "div" + i; removing the numbers
         div.addEventListener("mouseover", () => {
             div.style.backgroundColor = "red";
         });
         divContainer.appendChild(div);
     }
+
+    
 }
 
 function changeLayout () {
     let uservalue = prompt("Enter new layout value")
     console.log(uservalue)
+    totalSquares = uservalue * uservalue
+    console.log(totalSquares)
 
+    // clear grid
     while (divContainer.firstElementChild) { // while a firstelementchild exists
         divContainer.removeChild(divContainer.firstElementChild); // remove the first element child
     }
-    
 
+    // calc length and width of square 
 
-    // create new grid
-    for (let i = 1; i < uservalue; i++) {
+    let squareArea = 409600 / totalSquares;
+    console.log(squareArea)
+    let length = Math.sqrt(squareArea);
+    length = length - 4; // account for border
+    console.log(length);
+
+    // put each square in with the new measurements
+    for (let i = 0; i < totalSquares; i++) {
         const div = document.createElement('div');
         div.className = "div" + i;
         divContainer.appendChild(div);
+
+        document.querySelector('.div' + [i]).style.height = `${length}` + `px`;
+        document.querySelector('.div' + [i]).style.width = `${length}` + `px`;
+
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "red";
+        });
+
     }
 
-}
+};
 
 sixteendivs();
